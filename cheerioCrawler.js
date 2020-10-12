b9c0995fff5fd248dfbda4b021e52efd27735c14
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2020-10-12 14:03:51 -0400

    Add files via upload

--- cheerioCrawler.js
+++ cheerioCrawler.js
@@ -1,18 +1,37 @@
 const Apify = require('apify');
 
+const useUrls = ['truewindtechnology.com'];
+const purls = [];
+for (var u in useUrls) {
+    purls.push('http[s?]://'+useUrls[u]+'/[.+]');
+    purls.push('http[s?]://www.'+useUrls[u]+'/[.+]');        
+}
+
+
 Apify.main(async () => {
     // Prepare a list of URLs to crawl
     
     const requestQueue = await Apify.openRequestQueue();
-    await requestQueue.addRequest({ url: 'https://truewindtechnology.com' });    
+    for (var u in useUrls) {
+        await requestQueue.addRequest({ url: 'https://'+useUrls[u] });    
+    }
     
     // Crawl the URLs
     const crawler = new Apify.CheerioCrawler({
         requestQueue,
         handlePageFunction: async ({ request, response, body, contentType, $ }) => {
             const data = [];
+            const metas = [];
 
-            // Do some data extraction from the page with Cheerio.
+            $('head > meta').each((index, el) => {
+                console.log("meta element");
+                console.log(el.attribs);
+//                metas.push({
+//                    name: el.attr("name"),
+//                    content: el.attr("content"),
+//                });
+            });
+/*
             $('.jet-feature-panel').each((index, el) => {
                 data.push({
                     title: $(el)
@@ -20,20 +39,22 @@ Apify.main(async () => {
                         .text(),
                 });
             });
-
+*/
             // Save the data to dataset.
             await Apify.pushData({
                 url: request.url,
 //                html: body,
-                data,
+                title: $('title').text(), 
+                metas: metas, 
+                data: data,
             });
 
-            await Apify.utils.enqueueLinks({
-                $,
-                requestQueue,
-                baseUrl: request.loadedUrl,
-                pseudoUrls: ['http[s?]://truewindtechnology.com/[.+]', 'http[s?]://www.truewindtechnology.com/[.+]'],
-            });            
+//            await Apify.utils.enqueueLinks({
+//                $,
+//                requestQueue,
+//                baseUrl: request.loadedUrl,
+//                pseudoUrls: purls,
+//            });            
             
         },
     });
