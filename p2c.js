c8c6cb4a2869faefc306da73683be254f4ffeea3
Jonathan Tweedy <jetweedy@users.noreply.github.com>
2020-11-12 08:19:45 -0500

    Add files via upload

--- p2c.js
+++ p2c.js
@@ -40,7 +40,7 @@ var scrapeDetails = async (browser, url, n) => {
     // Select 10,000 and let it load up
     await page.waitForSelector("#pager_center > table > tbody > tr > td:nth-child(5) > select");
     await page.select('#pager_center > table > tbody > tr > td:nth-child(5) > select', '10000');
-	await page.waitForTimeout(1000);
+	await page.waitFor(1000);
 
     // Select the Nth row (as passed in params)
     await page.click("#tblII tbody tr:nth-child("+n+")");
@@ -82,18 +82,18 @@ var scrapeDetails = async (browser, url, n) => {
 
 
 var scrape = async (jail_id, url) => {
-	let browser = await puppeteer.launch({headless: false});	// <--- set to true for scraping
+	let browser = await puppeteer.launch({headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox']});	// <--- set to true for scraping
 	let page = await browser.newPage();
 	await page.goto(url);
     await page.waitForSelector("#pager_center > table > tbody > tr > td:nth-child(5) > select");
     await page.select('#pager_center > table > tbody > tr > td:nth-child(5) > select', '10000');
-	await page.waitForTimeout(1000);
+	await page.waitFor(1000);
     let data = await page.$$eval('#tblII tbody tr', tds => tds.map((td) => {
       return td.innerText;
     }));
     for (var d in data) {
         d = parseInt(d);
-        if (d < 1) {
+        if (d < 3) {
             let deets = await scrapeDetails(browser, url, d+1);
 //            console.log("deets", deets);
             var sql = "INSERT INTO jail_records (jail_id, name, age, sex, race, created_at, updated_at) VALUES (?,?,?,?,?,CURRENT_TIMESTAMP,CURRENT_TIMESTAMP)";
